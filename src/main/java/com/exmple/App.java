7c1ea2b7557e45f6c5eae45d4d1d90a4285e729c
yomikoyomi <yomi.koyomi@gmail.com>
2015-12-25 00:20:21 +0900

    first commit

---
+++ src/main/java/com/exmple/App.java
@@ -0,0 +1,13 @@
+package com.exmple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
